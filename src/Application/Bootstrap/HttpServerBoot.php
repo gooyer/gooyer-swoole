d6bff4e5227f9151578b6fa1c46839a08c970b23
Łukasz Grajewski <grajewski.lukasz@wp.pl>
2022-06-23 09:46:40 +0200

    Implements routes

--- src/Application/Bootstrap/HttpServerBoot.php
+++ src/Application/Bootstrap/HttpServerBoot.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Gooyer\Application\Bootstrap;
 
+use FastRoute\RouteCollector;
 use Gooyer\Contracts\Bootable;
 use Gooyer\Contracts\Application;
 use Gooyer\Application\Facades\Env;
@@ -13,11 +14,73 @@ use Swoole\Http\Server;
 
 class HttpServerBoot implements Bootable
 {
+    private function handleRequest($dispatcher, string $request_method, string $request_uri)
+    {
+        list($code, $handler, $vars) = $dispatcher->dispatch($request_method, $request_uri);
+
+        switch ($code) {
+            case \FastRoute\Dispatcher::NOT_FOUND:
+                $result = [
+                    'status' => 404,
+                    'message' => 'Not Found',
+                    'errors' => [
+                        sprintf('The URI "%s" was not found', $request_uri)
+                    ]
+                ];
+                break;
+            case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+                $allowedMethods = $handler;
+                $result = [
+                    'status' => 405,
+                    'message' => 'Method Not Allowed',
+                    'errors' => [
+                        sprintf('Method "%s" is not allowed', $request_method)
+                    ]
+                ];
+                break;
+            case \FastRoute\Dispatcher::FOUND:
+                $result = call_user_func($handler, $vars);
+                break;
+        }
+
+        return $result;
+    }
+
     public function bootRoutes(Application $application, Server $server): void
     {
         // Load routes
-        $server->on("onRequest", function(Request $request, Response $response) {
+        $server->on("request", function(Request $request, Response $response) {
+            $request_method = $request->server['request_method'];
+            $request_uri = $request->server['request_uri'];
 
+            // populate the global state with the request info
+            $_SERVER['REQUEST_URI'] = $request_uri;
+            $_SERVER['REQUEST_METHOD'] = $request_method;
+            $_SERVER['REMOTE_ADDR'] = $request->server['remote_addr'];
+
+            $_GET = $request->get ?? [];
+            $_FILES = $request->files ?? [];
+
+            // form-data and x-www-form-urlencoded work out of the box so we handle JSON POST here
+            if ($request_method === 'POST' && $request->header['content-type'] === 'application/json') {
+                $body = $request->rawContent();
+                $_POST = empty($body) ? [] : json_decode($body);
+            } else {
+                $_POST = $request->post ?? [];
+            }
+
+            // global content type for our responses
+            $response->header('Content-Type', 'application/json');
+            //Tutaj route
+            $dispatcher = \FastRoute\simpleDispatcher(function (RouteCollector $r) {
+                $r->addRoute('GET', '/[{title}]', 'get_index_handler');
+                $r->addRoute('POST', '/[{title}]', 'post_index_handler');
+            });
+
+            $result = $this->handleRequest($dispatcher, $request_method, $request_uri);
+
+            // write the JSON string out
+            $response->end(json_encode($result));
         });
     }
 
